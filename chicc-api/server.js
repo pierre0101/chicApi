dd17c61e056e9d34a78694aca4f9011c0a7eb0ad
pierre0101 <148709728+pierre0101@users.noreply.github.com>
2025-06-09 21:43:32 +0300

    API-ENDPOINT

--- chicc-api/server.js
+++ chicc-api/server.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // CORS configuration
-const allowedOrigins = ['https://chicapi.onrender.com'];
+const allowedOrigins = ['https://chic-lhtw.onrender.com/'];
 // const subnetRegex = /^http?:\/\/192\.168\.212\.\d{1,3}(:\d+)?$/;
 
 app.use(cors({
